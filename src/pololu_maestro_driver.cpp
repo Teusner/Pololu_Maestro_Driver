f540913c7f67eac87ca61b1c26858c2f563d3ee1
Teusner <quentin.brateau@ensta-bretagne.org>
2023-01-11 16:04:05 +0100

    Update

--- src/pololu_maestro_driver.cpp
+++ src/pololu_maestro_driver.cpp
@@ -4,10 +4,11 @@
 
 void PololuMaestroDriver::SetPosition(uint8_t channel, uint16_t position) {
     if (position >= min_channel_value_ && position <= max_channel_value_) {
+        uint16_t quarter_us = 4 * position;
         uint8_t command[4] = {
             0x84, channel,
-            (static_cast<uint16_t>(4 * position) & (uint8_t)0x7F),
-            ((static_cast<uint16_t>(4 * position) >> 7) & (uint8_t)0x7F)
+            (quarter_us & (uint8_t)0x7F),
+            (quarter_us >> 7) & (uint8_t)0x7F
         };
         serial_->write(sizeof(command), command, 1000);
     }
