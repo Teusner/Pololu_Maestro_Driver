fb74877d2e0e66d90d1a8f977ac964d02eabbddf
Teusner <quentin.brateau@ensta-bretagne.org>
2023-01-19 14:16:57 +0100

    Update

--- examples/multiple_positions.cpp
+++ examples/multiple_positions.cpp
@@ -18,5 +18,10 @@ int main() {
         std::rotate(values.begin(), values.begin()+1, values.end());
         driver.SetMultiplePositions(3, 1, &values[0]);
         std::this_thread::sleep_for(std::chrono::seconds(2));
+
+        std::size_t c = 0;
+        for (const auto v: values) {
+            std::cout << "Channel " << ++c << " " << v << std::endl;
+        }
     }
 }
